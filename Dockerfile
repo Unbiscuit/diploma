9f6ea5570f47dfd8fbb4767f9107eb8ecc683dec
Unbiscuit <bortov.maxim@yandex.ru>
2025-04-27 19:47:39 +0300

    new commit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM python:3.13-slim
 
+# Указываем рабочую директорию внутри контейнера
 WORKDIR /app
 
+# Копируем зависимости и устанавливаем
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Копируем весь проект внутрь контейнера
 COPY . .
 
-CMD ["python", "-m", "app.ingest_2_minio"]
+# Указываем команду запуска
+CMD ["python", "-m", "app.main"]
--- app/main.py
+++ app/main.py
@@ -1,18 +1,33 @@
 from concurrent import futures
 import grpc
+from fastapi import FastAPI
+import threading
+import uvicorn
 from grpc_reflection.v1alpha import reflection
 from app.nica_proto import ingest_pb2, ingest_pb2_grpc
-from app.producer import publish_event  
+from app.producer import publish_event
+
+# Создаём FastAPI приложение для health-чека
+health_app = FastAPI()
+
+@health_app.get("/healthz")
+def healthz():
+    return {"status": "ok"}
 
 class IngestServiceServicer(ingest_pb2_grpc.IngestServiceServicer):
     def IngestEvent(self, request, context):
         publish_event(request.detector, request.payload)
         return ingest_pb2.Ack()
 
+def start_http_server():
+    uvicorn.run(health_app, host="0.0.0.0", port=8080, log_level="warning")
+
 def serve():
+    # Параллельно запускаем HTTP сервер для пробы
+    threading.Thread(target=start_http_server, daemon=True).start()
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    ingest_pb2_grpc.add_IngestServiceServicer_to_server(
-        IngestServiceServicer(), server)
+    ingest_pb2_grpc.add_IngestServiceServicer_to_server(IngestServiceServicer(), server)
 
     SERVICE_NAMES = (
         'nica_proto.IngestService',
